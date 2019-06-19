baf783067689711e17a9fc5984cfb473771f3134
Samuel Lindgren <samuel@dynorobotics.se>
2019-06-20 01:51:28 +0200

    Use rosidl_generate_idl_interfaces

--- cmake/register_cs.cmake
+++ cmake/register_cs.cmake
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 Esteve Fernandez <esteve@apache.org>
+# Copyright 2015 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,15 +15,18 @@
 macro(rosidl_generator_cs_extras BIN GENERATOR_FILES TEMPLATE_DIR)
   find_package(ament_cmake_core QUIET REQUIRED)
   ament_register_extension(
-    "rosidl_generate_interfaces"
+    "rosidl_generate_idl_interfaces"
     "rosidl_generator_cs"
     "rosidl_generator_cs_generate_interfaces.cmake")
 
   normalize_path(BIN "${BIN}")
   set(rosidl_generator_cs_BIN "${BIN}")
 
-  normalize_path(GENERATOR_FILES "${GENERATOR_FILES}")
-  set(rosidl_generator_cs_GENERATOR_FILES "${GENERATOR_FILES}")
+  set(rosidl_generator_cs_GENERATOR_FILES "")
+  foreach(_generator_file ${GENERATOR_FILES})
+    normalize_path(_generator_file "${_generator_file}")
+    list(APPEND rosidl_generator_cs_GENERATOR_FILES "${_generator_file}")
+  endforeach()
 
   normalize_path(TEMPLATE_DIR "${TEMPLATE_DIR}")
   set(rosidl_generator_cs_TEMPLATE_DIR "${TEMPLATE_DIR}")
